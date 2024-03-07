0147d2e839b82f786a1ea45d177a0a8814c94860
Desmondflexy <ikilodesmond@gmail.com>
2024-03-08 00:11:41 +0100

    fix home images

--- server-side/src/models/token.ts
+++ server-side/src/models/token.ts
@@ -1,36 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 interface IToken extends mongoose.Document {
-  user: mongoose.Types.ObjectId;
-  otp: string;
-  type: string;
-  expires: Date;
+    user: mongoose.Types.ObjectId;
+    otp: string;
+    type: string;
+    expires: Date;
 }
 
-const tokenSchema = new mongoose.Schema<IToken>({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-  otp: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ['email', 'password']
-  },
-  expires: {
-    type: Date,
-    required: true,
-    default: Date.now() + 2 * 60 * 1000  // 2mins
-  }
-}, {
-  timestamps: true
-});
+const tokenSchema = new mongoose.Schema<IToken>(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        otp: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+            enum: ["email", "password"],
+        },
+        expires: {
+            type: Date,
+            required: true,
+            default: Date.now() + 2 * 60 * 1000, // 2mins
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-const Token = mongoose.model<IToken>('Token', tokenSchema);
+const Token = mongoose.model<IToken>("Token", tokenSchema);
 
 export default Token;
