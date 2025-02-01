adc386bacc30937d6efd2a1a38d702f3f07334e3
desmondflexy <ikilodesmond@gmail.com>
2025-02-01 01:51:12 +0100

    feat: add toggle for enabling mock API in AdminExtras component

--- client-side/src/components/account/admin/Extras.tsx
+++ client-side/src/components/account/admin/Extras.tsx
@@ -5,7 +5,12 @@ import { toast } from "react-toastify";
 
 export function AdminExtras() {
     const [orm, setOrm] = useState("");
+    const [mockApiEnabled, setMockApiEnabled] = useState(true);
     useEffect(getActiveOrm, []);
+
+    function handleMockApiChange(){
+        setMockApiEnabled(!mockApiEnabled);
+    }
 
     function getActiveOrm() {
         apiService.getAppInfo().then(res => {
@@ -14,11 +19,18 @@ export function AdminExtras() {
             toastError(err, toast);
         });
     }
+
     return (
         <div>
             <h3>Extras</h3>
             <li>Active ORM: {orm}</li>
             <li>Other settings</li>
+            <li>
+                <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked={mockApiEnabled} onChange={handleMockApiChange} />
+                    <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Enable Mock Api</label>
+                </div>
+            </li>
             <p style={{ color: "red" }}>Coming soon...</p>
         </div>
     )
